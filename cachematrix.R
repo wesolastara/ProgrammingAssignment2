7aa51a32a1b84398c6401ee988541ac325f68775
wesolastara <maciejgielnik@gmail.com>
2020-07-19 22:47:57 +0200

    two more comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,10 @@
 ## This function prepares values for the cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  m <- NULL    ## a flag for checking if inversion was calculated is set to NULL
   setmatrix <- function(y) {   ## setting matrix
     x <<- y
-    m <<- NULL
+    m <<- NULL ## a flag for checking if inversion was calculated is set to NULL
   }
   getmatrix <- function() x    ## getting matrix 
   setsolve <- function(solve) m <<- solve   ## setting inversion of a matrix 
